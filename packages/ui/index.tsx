ca7c5b921bf7058ba79a03dcf7942cb4f266d789
henry <70426440+hyjoong@users.noreply.github.com>
2024-10-31 12:58:30 +0900

    feat: badge component packages/ui 경로로 이동 및 적용

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -3,5 +3,6 @@ export * from "@ui/components/ui/card";
 export * from "@ui/components/ui/label";
 export * from "@ui/components/ui/input";
 export * from "@ui/components/ui/breadcrumb";
+export  * from "@ui/components/ui/badge";
 
 export { cn } from "@ui/lib/utils";
