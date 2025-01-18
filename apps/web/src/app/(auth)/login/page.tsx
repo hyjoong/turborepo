f9a69666ad9713192a8dafc286236e76ca8fe87d
hyjoong <maplesosser@naver.com>
2025-01-18 14:54:32 +0900

    chore: remove unused apps/web project

--- apps/web/src/app/(auth)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/app/components/auth/LoginForm";
-
-export default function SignUpPage() {
-  return (
-    <div className="container flex flex-col justify-center min-h-screen gap-5">
-      <LoginForm />
-    </div>
-  );
-}
--- apps/web/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-import config from "@repo/ui/tailwind.config";
-
-const webConfig = {
-  ...config,
-  presets: [config],
-} satisfies Config;
-
-export default webConfig;
